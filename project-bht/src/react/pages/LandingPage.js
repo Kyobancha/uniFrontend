679c3668f2d534589ee2a3829173f8367ae62855
Timo <timo.huynh@tutamail.com>
2022-06-02 10:32:21 +0200

    working on landing-page. background image should be replaced with gradient for now

--- project-bht/src/react/pages/LandingPage.js
+++ project-bht/src/react/pages/LandingPage.js
@@ -3,9 +3,8 @@ import image from "../../img/landingpage-background.jpg";
 function LandingPage(props) {
     return (
         <div>
-            <div className="hero-pattern"></div>
-            <h1>Login</h1>
-            {/* <img alt="a stacker game" src={image} /> */}
+            <h1>{props.children}</h1>
+            {/* <div style={background-image: url(${image})}  /> */}
         </div>
     );
 }
