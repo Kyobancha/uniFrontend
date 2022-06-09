9397ec8906a1f43ea3e1c36f532a323c2cf4b4f8
Timo <huynh@Timos-MacBook-Air.local>
2022-06-09 15:14:04 +0200

    login modal can now be closed and not just opened

--- src/App.js
+++ src/App.js
@@ -9,18 +9,22 @@ import LoginModal from "./react/components/LoginModal";
 function App() {
     const [modalOpen, setModalOpen] = useState(false)
 
-    function toggleModal(){
-        setModalOpen(!modalOpen)
+    function closeModal(){
+        setModalOpen(false)
+    }
+
+    function openModal(){
+        setModalOpen(true)
     }
 
     return (
         <div className="h-100% bg-gradient-to-r from-cyan-400 to-blue-500">
             <div className="flex space-between justify-between">
-                <h3>Cool Forum</h3>
-                <button to="/login" onClick={toggleModal}>Login</button>
+                <h3>uni Frontend Forum</h3>
+                <button to="/login" onClick={openModal}>Login</button>
             </div>
             <Navigation/>
-            {modalOpen ? <LoginModal toggleModal={toggleModal}></LoginModal> : null}
+            {modalOpen ? <LoginModal modalOpen={modalOpen} closeModal={closeModal}></LoginModal> : null}
             <Outlet>
                 <h1>test</h1>
                 <Home>Hello world</Home>
--- src/react/components/LoginModal.js
+++ src/react/components/LoginModal.js
@@ -1,8 +1,10 @@
-function LoginModal() {
+import { useState } from "react";
+
+function LoginModal(props) {
     return (
         <div className="absolute top-0 bg-gray-700 bg-opacity-50 bg-repeat w-screen h-screen">
             <div className="sticky ml-auto mr-auto left-0 right-0 top-1/4 text-center bg-blue-300 w-3/12 h-2/6 drop-shadow-xl flex flex-col">
-                <button className="bg-red-500 border-2 rounded w-8 h-8 drop-shadow-md self-end">X</button>
+                <button onClick={props.closeModal} className="bg-red-500 border-2 rounded w-8 h-8 drop-shadow-md self-end">X</button>
                 <h3 className="font-bold mt-2">Login</h3>
                 <form>
                     <div className="flex flex-row justify-center mt-2">
